c752fd9db85868b71abcf27a9d84868b0d4199bf
M Kim <minskim010203@gmail.com>
2024-02-18 15:45:16 +0000

    eval script

--- scripts/evaluation.sh
+++ scripts/evaluation.sh
@@ -1,6 +1,17 @@
 python eval.py \
-    --model_name=meta=llama/Llama-2-7b-hf \
+    --model_name=meta-llama/Llama-2-7b-hf \
     --bf16=1 \
-    --load_checkpoint_path=/path/to/checkpoint \
-    --test_batch_size=2 \
-    --train_type=
+    --load_checkpoint_path=ckpt/ehrsql-2024/SFT/festive-bao-5 \
+    --test_batch_size=1 \
+    --train_type=SFT
+
+wait
+
+python eval.py \
+	--model_name=meta-llama/Llama-2-7b-hf \
+	--bf16=1 \
+	--load_adapter_path=ckpt/ehrsql-2024/SFT/festive-bao-5 \
+	--load_checkpoint_path=ckpt/ehrsql-2024/PPO/incandescent-ox-24 \
+	--test_batch_size=1 \
+	--train_type=PPO
+
