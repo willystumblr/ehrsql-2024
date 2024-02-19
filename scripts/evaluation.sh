377e47650bd15ed90bdb41eee8670e25038b8a57
M Kim <minskim010203@gmail.com>
2024-02-19 09:30:38 +0900

    example script

--- scripts/evaluation.sh
+++ scripts/evaluation.sh
@@ -1,11 +1,11 @@
-python eval.py \
-    --model_name=meta-llama/Llama-2-7b-hf \
-    --bf16=1 \
-    --load_checkpoint_path=ckpt/ehrsql-2024/SFT/festive-bao-5 \
-    --test_batch_size=1 \
-    --train_type=SFT
+# python eval.py \
+#     --model_name=meta-llama/Llama-2-7b-hf \
+#     --bf16=1 \
+#     --load_checkpoint_path=ckpt/ehrsql-2024/SFT/festive-bao-5 \
+#     --test_batch_size=1 \
+#     --train_type=SFT
 
-wait
+# wait
 
 python eval.py \
 	--model_name=meta-llama/Llama-2-7b-hf \
