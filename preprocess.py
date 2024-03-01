f2c182f6405915277570d5e5a4b817fc1092a583
M Kim <minskim010203@gmail.com>
2024-03-01 14:15:27 +0900

    preprocess; answers

--- preprocess.py
+++ preprocess.py
@@ -7,12 +7,10 @@ from sklearn.model_selection import train_test_split
 from utils.data_io import (
     DB_ID,
     BASE_DATA_DIR,
-    BASE_CKPT_DIR,
-    TABLES_PATH,
+    TRAIN_ANSWER_PATH,
     TRAIN_DATA_PATH,
     TRAIN_LABEL_PATH,
     VALID_DATA_PATH,
-    DB_PATH
 )
 
 
@@ -22,6 +20,7 @@ from utils.data_io import write_json as write_data
 # Load train and validation sets
 train_data = read_data(TRAIN_DATA_PATH)
 train_label = read_data(TRAIN_LABEL_PATH)
+train_answer = read_data(TRAIN_ANSWER_PATH)
 valid_data = read_data(VALID_DATA_PATH)
 
 
@@ -40,6 +39,7 @@ new_train_keys, new_valid_keys = train_test_split(
 # Initialize containers for the new training and validation sets
 new_train_data = []
 new_train_label = {}
+new_train_answer = {}
 new_valid_data = []
 new_valid_label = {}
 
@@ -48,6 +48,7 @@ for sample in train_data['data']:
     if sample['id'] in new_train_keys:
         new_train_data.append(sample)
         new_train_label[sample['id']] = train_label[sample['id']]
+        new_train_answer[sample['id']] = train_answer[sample['id']]
     elif sample['id'] in new_valid_keys:
         new_valid_data.append(sample)
         new_valid_label[sample['id']] = train_label[sample['id']]
@@ -62,10 +63,11 @@ new_valid_data = {'version': f'{DB_ID}_sample', 'data': new_valid_data}
 # Set directory for the new splitted data
 NEW_TRAIN_DIR = os.path.join(BASE_DATA_DIR, '__train')
 NEW_VALID_DIR = os.path.join(BASE_DATA_DIR, '__valid')
-NEW_TEST_DIR = os.path.join(BASE_DATA_DIR, 'valid')
+# NEW_TEST_DIR = os.path.join(BASE_DATA_DIR, 'valid')
 
 # Save the new datasets to JSON files for later use
 write_data(os.path.join(NEW_TRAIN_DIR, "data.json"), new_train_data)
 write_data(os.path.join(NEW_TRAIN_DIR, "label.json"), new_train_label)
+write_data(os.path.join(NEW_TRAIN_DIR, "answer.json"), new_train_answer)
 write_data(os.path.join(NEW_VALID_DIR, "data.json"), new_valid_data)
 write_data(os.path.join(NEW_VALID_DIR, "label.json"), new_valid_label)
--- utils/data_io.py
+++ utils/data_io.py
@@ -13,12 +13,14 @@ RESULT_DIR = f'{BASE_DIR}/result_submission/'
 SCORE_PROGRAM_DIR = f'{BASE_DIR}/scoring_program/'
 
 # File paths for the dataset and labels
-TABLES_PATH = os.path.join(BASE_DATA_DIR, 'tables.json')               # JSON containing database schema
-TRAIN_DATA_PATH = os.path.join(BASE_DATA_DIR, 'train', 'data.json')    # JSON file with natural language questions for training data
-TRAIN_LABEL_PATH = os.path.join(BASE_DATA_DIR, 'train', 'label.json')  # JSON file with corresponding SQL queries for training data
-VALID_DATA_PATH = os.path.join(BASE_DATA_DIR, 'valid', 'data.json')    # JSON file for validation data
-DB_PATH = os.path.join(BASE_DATA_DIR, f'{DB_ID}.sqlite')               # Database path
+TABLES_PATH = os.path.join(BASE_DATA_DIR, 'tables.json')                # JSON containing database schema
+TRAIN_DATA_PATH = os.path.join(BASE_DATA_DIR, 'train', 'data.json')     # JSON file with natural language questions for training data
+TRAIN_LABEL_PATH = os.path.join(BASE_DATA_DIR, 'train', 'label.json')   # JSON file with corresponding SQL queries for training data
+TRAIN_ANSWER_PATH = os.path.join(BASE_DATA_DIR, 'train', 'answer.json') # JSON file with fetched answers for eah SQL query for training data
+VALID_DATA_PATH = os.path.join(BASE_DATA_DIR, 'valid', 'data.json')     # JSON file for validation data
+DB_PATH = os.path.join(BASE_DATA_DIR, f'{DB_ID}.sqlite')                # Database path
 
+# Will be deprecated during test phase
 NEW_TRAIN_DIR = os.path.join(BASE_DATA_DIR, '__train')
 NEW_VALID_DIR = os.path.join(BASE_DATA_DIR, '__valid')
 NEW_TEST_DIR = os.path.join(BASE_DATA_DIR, 'valid')
