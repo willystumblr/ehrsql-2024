c06ecfcf2485100ef78f4b7ba40e139234b66087
M Kim <minskim010203@gmail.com>
2024-03-05 21:06:20 +0900

    setup.py edited

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ setup(
     version='0.1',
     packages=find_packages(include=['preprocess', 'scoring_program', 'utils', 'ingestion_program']),
     install_requires=[
-        'pyarrow==14.0.2', 
-        'transformers==4.37.2',
-        'peft==0.8.2',
-        'trl==0.7.10',
+        'sqlparse==0.4.4',
+        'scikit-learn==1.4.1.post1', 
+        'transformers==4.38.2',
+        'peft==0.9.0',
+        'trl==0.7.11',
         'wandb==0.16.3',
-        'torchaudio==2.1.0',
-        'torchvision==0.16.0',
         'xformers==0.0.24',
+        'accelerate==0.27.2',
         'bitsandbytes==0.42.0',
         'unsloth[conda] @ git+https://github.com/unslothai/unsloth.git'
     ]
