aa10bc06950f8d98569b77d17b3a51a98393025a
M Kim <minskim010203@gmail.com>
2024-03-11 23:03:19 +0900

    model push to hub

--- ppo.py
+++ ppo.py
@@ -313,4 +313,4 @@ if __name__=="__main__":
 
     os.makedirs(save_path, exist_ok=True)
     # ppo_trainer.save_pretrained(save_path)
-    ppo_trainer.push_to_hub(f"{args.project_name}-{args.model_name.split('/')[-1]}", token=HF_W_TOKEN)
+    model.push_to_hub(f"{args.project_name}-{args.model_name.split('/')[-1]}", token=HF_W_TOKEN)
