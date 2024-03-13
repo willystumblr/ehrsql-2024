a8d27cc0d75c45bfe31fe034cc61986f5ea58ff8
M Kim <minskim010203@gmail.com>
2024-03-13 23:13:27 +0900

    whole data ppo

--- ppo.py
+++ ppo.py
@@ -191,14 +191,17 @@ if __name__=="__main__":
 
     train_data, valid_data, test_data = build_dataset(args.phase)
 
-
-    logger.info("*** Sampling PPO Datasets... ***")
-    train_sample = train_data.select(random.sample(range(len(train_data)), args.num_samples))
-    null_count = len(list(filter(lambda x: x['label']=='null', train_sample)))
-    while (null_count > args.num_samples*0.2) or (null_count < args.num_samples*0.05):
+    if args.num_samples>1:
+        logger.info("*** Sampling PPO Datasets... ***")
         train_sample = train_data.select(random.sample(range(len(train_data)), args.num_samples))
+        null_count = len(list(filter(lambda x: x['label']=='null', train_sample)))
+        while (null_count > args.num_samples*0.2) or (null_count < args.num_samples*0.05):
+            train_sample = train_data.select(random.sample(range(len(train_data)), args.num_samples))
+        ppo_dataset = train_sample.map(create_ppo_prompt)
+    else:
+        ppo_dataset = train_data.map(create_ppo_prompt)
     
-    ppo_dataset = train_sample.map(create_ppo_prompt)
+
     # ppo_dataset = ppo_dataset.rename_column("question", "query")
     ppo_dataset = ppo_dataset.remove_columns(["id"])
 
