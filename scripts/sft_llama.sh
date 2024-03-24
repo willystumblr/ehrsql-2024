b2b386644de48cac737401bae21105968738f519
M Kim <minskim010203@gmail.com>
2024-03-24 15:12:09 +0000

    updated ex scripts

--- scripts/sft_llama.sh
+++ scripts/sft_llama.sh
@@ -1,16 +1,17 @@
-python -m torch.distributed.launch --nproc_per_node 2 sft.py \
-    --train_type=SFT \
-    --project_name=ehrsql-2024-sft \
-    --model_name=meta-llama/Llama-2-7b-hf \
+python sft.py \
+    --train_type=text2sql \
+    --base_model_name=google/gemma-2b-it \
+    --project_name=ehrsql-2024-sft-text2sql\
     --train_epochs=3 \
     --train_batch_size=8 \
-    --valid_batch_size=4 \
     --learning_rate=1e-3 \
     --logging_steps=10 \
     --lr_scheduler_type=cosine \
     --bf16=1 \
     --db_id=mimic_iv \
-    --evaluation_strategy=epoch \
+    --evaluation_strategy=no \
     --test_batch_size=1 \
     --save_strategy=epoch \
-    --load_best_model_at_end=True
+    --max_new_token=512 \
+    --adapter_config_path=adapter_config/gemma.json \
+    --phase=dev_final
