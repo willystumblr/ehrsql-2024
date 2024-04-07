5fc528c5ace8239681e49d13859e0ad4ae269f8e
M Kim <minskim010203@gmail.com>
2024-04-07 07:31:16 +0000

    eval script update

--- scripts/evaluation.sh
+++ scripts/evaluation.sh
@@ -1,16 +1,7 @@
-# python eval.py \
-#     --model_name=meta-llama/Llama-2-7b-hf \
-#     --bf16=1 \
-#     --load_checkpoint_path=ckpt/ehrsql-2024/SFT/festive-bao-5 \
-#     --test_batch_size=1 \
-#     --train_type=SFT
-
-# wait
-
-python eval.py \
-	--model_name=meta-llama/Llama-2-7b-hf \
+python eval_multi.py \
+	--model_name=willystumblr/ehrsql-2024-sft-text2sql-gemma-2b-it \
 	--bf16=1 \
-	--load_checkpoint_path=ckpt/PPO/incandescent-ox-24 \
 	--test_batch_size=1 \
-	--train_type=PPO
+	--train_type=text2sql \
+	--phase=dev
 
